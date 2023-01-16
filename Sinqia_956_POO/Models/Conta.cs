4de8cabd0d04002913204a49509f296d142aeeb9
Rodrigo <rodrigo.freire@msn.com>
2023-01-16 16:07:06 -0300

    Criando os métodos 'Sacar', 'Transferir' e 'Extrato'

--- Sinqia_956_POO/Models/Conta.cs
+++ Sinqia_956_POO/Models/Conta.cs
@@ -6,11 +6,39 @@ using System.Threading.Tasks;
 
 namespace Sinqia_956_POO.Models
 {
-    public abstract class Conta
+    public class Conta
     {
-        public Conta() 
-        {
+        private double saldo { get; set; }
+        public string titular { get; set; }
+        protected int num_conta { get; set; }
 
+        public Conta(double saldo, string titular, int num_conta)
+        {
+            this.saldo = saldo;
+            this.titular = titular;
+            this.num_conta = num_conta;
+        }
+        public double Sacar(double valorSacado)
+        {
+            if (saldo > 0)
+            {
+                saldo-= valorSacado;
+            }
+            return saldo;
+        }
+
+        public double Transferir(double valorTransferencia)
+        {
+            if (num_conta > 0)
+            {
+                saldo-= valorTransferencia;
+            }
+            return saldo;
+        }
+
+        public void extrato()
+        {
+            Console.WriteLine($"O saldo atual é {saldo.ToString()}");
         }
     }
 }
--- Sinqia_956_POO/Program.cs
+++ Sinqia_956_POO/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Sinqia_956_POO
+﻿using Sinqia_956_POO.Models;
+
+namespace Sinqia_956_POO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            Conta conta = new Conta(100, "Rodrigo", 123456);
+
+            conta.extrato();
+            Console.WriteLine("\nRealizando Saque...");
+            conta.Sacar(9.90);
+            Console.WriteLine("\nSaque efetuado!\n");
+            conta.extrato();
+            Console.WriteLine("\n Realizando Transferência...");
+            conta.Transferir(40);
+            Console.WriteLine("\nTransferência conluída!\n");
+            conta.extrato();
+
         }
     }
 }
