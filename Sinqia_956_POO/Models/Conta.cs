4065f03db21d1b398d5bb9fad222bd5dd8e2fb57
Rodrigo <rodrigo.freire@msn.com>
2023-01-16 12:51:21 -0300

    Criando a Class Conta

---
+++ Sinqia_956_POO/Models/Conta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinqia_956_POO.Models
+{
+    public abstract class Conta
+    {
+        public Conta() 
+        {
+
+        }
+    }
+}
