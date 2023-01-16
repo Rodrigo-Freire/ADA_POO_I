9e6cc5d756d4ff8e624ed8f69a1e2d0ad8f7db22
Rodrigo <rodrigo.freire@msn.com>
2023-01-16 12:44:46 -0300

    Criação da Solution e Add .gitignore

---
+++ Sinqia_956_POO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sinqia_956_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
