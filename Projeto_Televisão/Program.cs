7be5912e32514100eb1cb7c0d2ff861d6ecd38ad
Rodrigo <rodrigo.freire@msn.com>
2023-01-25 12:20:06 -0300

    Criando a Class TV_Com_construtor

--- Projeto_Televisão/Program.cs
+++ Projeto_Televisão/Program.cs
@@ -23,6 +23,26 @@
             televisao.DiminuirVolume(9);
             televisao.DiminuirVolume(10);
 
+            TV_Com_Construtor televisao2 = new TV_Com_Construtor(0, 0, "Off");
+
+            Console.WriteLine();
+            Console.WriteLine("Iniciando TV2");
+            televisao2.Ligar();
+            televisao2.Ligar();
+            televisao2.Desligar();
+            televisao2.Desligar();
+            televisao2.Ligar();
+            televisao2.MudarCanal(13);
+            televisao2.MudarCanal(04);
+            televisao2.AumentarVolume(1);
+            televisao2.AumentarVolume(50);
+            televisao2.AumentarVolume(60);
+            televisao2.AumentarVolume(10);
+            televisao2.DiminuirVolume(1);
+            televisao2.DiminuirVolume(90);
+            televisao2.DiminuirVolume(9);
+            televisao2.DiminuirVolume(10);
+
 
             Console.ReadKey();
         }
---
+++ Projeto_Televisão/TV_Com_Construtor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Televisão
+{
+    public class TV_Com_Construtor : TV
+    {
+        public TV_Com_Construtor(int canal, int volume, string ligada)
+        {
+            this.canal = canal;
+            this.volume = volume;
+            this.ligada = ligada;
+        }
+
+    }
+}
