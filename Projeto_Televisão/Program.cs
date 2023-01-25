93bab3b64eaf2d4c7e9248622b35fe9bb834f2d7
Rodrigo <rodrigo.freire@msn.com>
2023-01-25 12:01:20 -0300

    Criando o Projeto Televisão

---
+++ Projeto_Televisão/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Projeto_Televisão
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            TV televisao= new TV();
+
+            televisao.Ligar();
+            televisao.Ligar();
+            televisao.Desligar();
+            televisao.Desligar();
+            televisao.Ligar();
+            televisao.MudarCanal(13);
+            televisao.MudarCanal(04);
+            televisao.AumentarVolume(1);
+            televisao.AumentarVolume(50);
+            televisao.AumentarVolume(60);
+            televisao.AumentarVolume(10);
+            televisao.DiminuirVolume(1);
+            televisao.DiminuirVolume(90);
+            televisao.DiminuirVolume(9);
+            televisao.DiminuirVolume(10);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projeto_Televisão/TV.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Televisão
+{
+    public class TV
+    {
+        public int canal { get; set; }
+        public int volume { get; set; }
+        public string? ligada { get; set; }
+
+        public string Ligar()
+        {
+            if (ligada != "On")
+            {
+                ligada = "On";
+                Console.WriteLine($"O status da TV é {ligada} e ela esta ligada.");
+                
+            }
+            else
+            {
+                Console.WriteLine("TV já esta ligada");
+            }
+            return ligada;
+        }
+
+        public string Desligar()
+        {
+            if (ligada == "On")
+            {
+                ligada = "Off";
+                Console.WriteLine($"O status da TV é {ligada} e ela esta desligada.");
+            }
+            else
+            {
+                Console.WriteLine("TV já esta desligada");
+            }
+            return ligada;
+        }
+
+        public int MudarCanal (int canal)
+        {
+            if (this.ligada == "Off")
+            {
+                Console.WriteLine("Não é possível mudar o canal! TV esta desligada");
+            }
+            else
+            {
+                this.canal = canal;
+                Console.WriteLine($"TV sintonizada no canal {canal}");
+            }
+            return canal;
+        }
+
+        public int AumentarVolume(int volume)
+        {
+            if (this.ligada == "Off")
+            {
+                Console.WriteLine("Não é possível aumentar o volume! TV esta desligada");
+            }
+            else
+            {
+                if (this.volume < 100)
+                {
+                    this.volume += volume;
+                    if (this.volume > 100)
+                    {
+                        this.volume = 100;
+                        Console.WriteLine("Volume Máximo!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Volume ajustado para {this.volume}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("TV já está no volume máximo!");
+                }
+            }            
+            return volume;
+        }
+
+        public int DiminuirVolume(int volume)
+        {
+            if (this.ligada == "Off")
+            {
+                Console.WriteLine("Não é possível diminuir o volume! TV esta desligada");
+            }
+            else
+            {
+                if (this.volume > 0)
+                {
+                    this.volume -= volume;
+                    if (this.volume <= 0)
+                    {
+                        this.volume = 0;
+                        Console.WriteLine("Volume Mínimo!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Volume ajustado para {this.volume}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("TV já esta no volume mínimo!");
+                }
+            }            
+            return volume;
+        }
+    }
+}
